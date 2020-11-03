27e4cd15d2102fda29528cc9f15beb79bf8f9261
pjach <paulius.jachi@gmail.com>
2020-11-03 18:21:35 +0200

    Initial commit

---
+++ McEliece.Cryptosystem.Security/McEliece.Cryptosystem.Security/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace McEliece.Cryptosystem.Security
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, projektinis!");
+        }
+    }
+}
